8c7283344cc3b6133436f15475110a7e939f8ed3
Niel Müller <niel.mueller@mgb.ch>
2023-05-03 14:54:47 +0200

    finish all sql_function execpt update_product but there are not tested

--- src/main/java/com/example/demo/sql_functions.java
+++ src/main/java/com/example/demo/sql_functions.java
@@ -6,7 +6,73 @@ import java.sql.*;
 
 public class sql_functions {
 
-    public String getProducts_of_list(Integer id_list) {
+
+    //_________________________________________________________________________________________________________________________________________
+    // all function for the shopping list
+    //_________________________________________________________________________________________________________________________________________
+    public String get_list_of_shopping_list() {
+        JSONObject results = new JSONObject();
+        // Verbindung zur SQLite-Datenbank herstellen
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:db.db")) {
+            // SELECT-Befehl ausführen
+
+            try (Statement stmt = conn.createStatement()) {
+                ResultSet rs = stmt.executeQuery("SELECT * FROM shopping_list");
+                while (rs.next()) {
+                    results.put("ID", rs.getInt("ID"));
+                    results.put("Shop", rs.getString("shop"));
+                    results.put("Name", rs.getString("name"));
+                }
+            }
+        } catch (SQLException e) {
+        }
+        return results.toString();
+    }
+
+    public String create_shopping_list(String name) {
+        String feedback = "delay_error";
+        // Verbindung zur SQLite-Datenbank herstellen
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:db.db")) {
+            // SELECT-Befehl ausführen
+            String sql = "INSERT INTO shopping_list(name) VALUES(?)";
+            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                // Werte für die Platzhalter setzen
+                pstmt.setString(1, name);
+                // INSERT-Befehl ausführen
+                pstmt.executeUpdate();
+                feedback = "success";
+            }
+        } catch (SQLException e) {
+            feedback="error";
+        }
+        return feedback;
+    }
+
+    public String delete_shopping_list(Integer ID) {
+        String feedback = "delay_error";
+        // Verbindung zur SQLite-Datenbank herstellen
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:db.db")) {
+            // SELECT-Befehl ausführen
+            String sql = "DELETE FROM shopping_list WHERE ID = ?;";
+            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                // Werte für die Platzhalter setzen
+                pstmt.setInt(1, ID);
+                // INSERT-Befehl ausführen
+                pstmt.executeUpdate();
+                feedback = "success";
+            }
+        } catch (SQLException e) {
+            feedback="error";
+        }
+        return feedback;
+    }
+
+
+
+    //_________________________________________________________________________________________________________________________________________
+    // all function for the products
+    //_________________________________________________________________________________________________________________________________________
+    public String getProducts_of_shopping_list(Integer id_list) {
         JSONObject results = new JSONObject();
         // Verbindung zur SQLite-Datenbank herstellen
         try (Connection conn = DriverManager.getConnection("jdbc:sqlite:db.db")) {
@@ -24,4 +90,66 @@ public class sql_functions {
         }
         return results.toString();
     }
+    public String create_product(String name, Integer ID_of_shopping_list, Integer Amount) {
+        String feedback = "delay_error";
+        // Verbindung zur SQLite-Datenbank herstellen
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:db.db")) {
+            // SELECT-Befehl ausführen
+            String sql = "INSERT INTO shopping_list(name, FK_shopping_list, Anzahl) VALUES(?, ?, ?)";
+            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                // Werte für die Platzhalter setzen
+                pstmt.setString(1, name);
+                pstmt.setInt(2, ID_of_shopping_list);
+                pstmt.setInt(3, Amount);
+                // INSERT-Befehl ausführen
+                pstmt.executeUpdate();
+                feedback = "success";
+            }
+        } catch (SQLException e) {
+            feedback="error";
+        }
+        return feedback;
+    }
+
+    public String delete_product(Integer ID) {
+        String feedback = "delay_error";
+        // Verbindung zur SQLite-Datenbank herstellen
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:db.db")) {
+            // SELECT-Befehl ausführen
+            String sql = "DELETE FROM products WHERE ID = ?;";
+            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                // Werte für die Platzhalter setzen
+                pstmt.setInt(1, ID);
+                // INSERT-Befehl ausführen
+                pstmt.executeUpdate();
+                feedback = "success";
+            }
+        } catch (SQLException e) {
+            feedback="error";
+        }
+        return feedback;
+    }
+
+    public String update_product(String name, Integer ID_of_product, Integer Amount) {
+        String feedback = "delay_error";
+        // Verbindung zur SQLite-Datenbank herstellen
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:db.db")) {
+            // SELECT-Befehl ausführen
+            String sql = "INSERT INTO shopping_list(name, , Anzahl) VALUES(?, ?, ?)";
+            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                // Werte für die Platzhalter setzen
+                pstmt.setString(1, name);
+                pstmt.setInt(2, ID_of_product);
+                pstmt.setInt(3, Amount);
+                // INSERT-Befehl ausführen
+                pstmt.executeUpdate();
+                feedback = "success";
+            }
+        } catch (SQLException e) {
+            feedback="error";
+        }
+        return feedback;
+    }
+
+
 }
