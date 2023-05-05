9c866add40f3af338bee29e3e22a16e52464c9ec
Niel Müller <niel.mueller@mgb.ch>
2023-05-05 09:27:50 +0200

    made functions for mapping put and delete /product

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
+import static com.example.demo.sql_functions.*;
+
 @RestController
 public class MainController {
 	/**
@@ -57,15 +59,15 @@ public class MainController {
 	 *	Here you can edit a product from a List.
 	 */
 	@PutMapping("/product")
-	public String editProduct() {
-		return "Hello DELETE";
+		public String editProduct(@RequestParam(value = "product_id", required = true) Integer product_id, @RequestParam(value = "product_name", required = true) String product_name, @RequestParam(value = "amount", required = true) Integer amount) {
+			return update_product(product_name,product_id, amount);
 	}
 	/**
 	 *	Here you can delete a product from a List.
 	 */
 	@DeleteMapping("/product")
-	public String deleteProduct() {
-		return "Hello EDIT";
+	public String deleteProduct(@RequestParam(value = "id", required = true) String id) {
+		return delete_product(Integer.parseInt(id));
 	}
 
 }
