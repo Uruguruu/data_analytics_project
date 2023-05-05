b825fc0f08087f22420dd33c89edb064288e4179
Rilind.Haradini <rilind.haradini@edu.tbz.ch>
2023-05-05 13:13:37 +0200

    fixed httpServletRequest

--- src/main/java/com/example/demo/views/ProductViewFunc.java
+++ src/main/java/com/example/demo/views/ProductViewFunc.java
@@ -1,10 +1,11 @@
 package com.example.demo.views;
 
+import com.vaadin.flow.server.VaadinServletRequest;
 import jakarta.servlet.http.HttpServletRequest;
 import com.example.demo.views.Fetchfunc;
 public class ProductViewFunc {
     private String[][] getListItems() {
-        HttpServletRequest request = ...
+        HttpServletRequest request = VaadinServletRequest.getCurrent().getHttpServletRequest();
         String id = request.getParameter("id");
         Fetchfunc.generalFetch("/products","id="+);
         return new String[][]{{"Volvo", "BMW", "Ford", "Mazda"}};
